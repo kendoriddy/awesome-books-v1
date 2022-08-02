41d4e4b96a5face5824b198702cfb5cd0365645b
Kehinde Onifade <onifkay@gmail.com>
2022-08-02 10:16:22 +0100

    Add funtion to didplay added books
    rendering the html files using javascript

--- js/index.js
+++ js/index.js
@@ -12,3 +12,27 @@ function errorMsg(message) {
     document.querySelector('.warning').textContent = '';
   }, 2000);
 }
+
+function showBooks(id, title, author) {
+    const items = document.createElement('div');
+    items.innerHTML = `
+      <p>${title}</p>
+      <p>${author}</p>
+      `;
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+    removeBtn.id = id;
+    const itemLine = document.createElement('hr');
+    items.append(removeBtn, itemLine);
+    removeBtn.addEventListener('click', () => {
+      books = books.filter((book) => {
+        if (book.id === id) {
+          return false;
+        }
+        return true;
+      });
+      localStorage.setItem('books', JSON.stringify(books));
+      items.remove();
+    });
+    list.appendChild(items);
+  }

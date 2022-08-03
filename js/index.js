d4ea8bd4a8e4419e768267adfafcd3e98636f700
Kehinde Onifade <onifkay@gmail.com>
2022-08-03 10:36:49 +0100

    add function to display book to UI

--- js/index.js
+++ js/index.js
@@ -45,3 +45,26 @@ class Book {
     localStorage.setItem('books', JSON.stringify(books));
   }
 }
+
+// Display books
+function showBooks(title, author, id) {
+  const bookList = document.querySelector('.book-list');
+  const items = document.createElement('li');
+  items.innerHTML = `
+      <div class="item-list"><h3><q>${title}</q></h3>
+    <h3> by </h3>
+    <h3> ${author}</h3></div>
+      `;
+  const removeBtn = document.createElement('button');
+  removeBtn.textContent = 'Remove';
+  removeBtn.className = 'remove-btn';
+  items.append(removeBtn);
+  bookList.appendChild(items);
+  
+  removeBtn.addEventListener('click', () => {
+    const book = new Book(title, author, id);
+    id = removeBtn.id;
+    book.remove();
+    items.remove();
+  });
+}

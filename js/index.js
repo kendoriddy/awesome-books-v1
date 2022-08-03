c419662b2767ef216067b11658a7002603058c0c
Kehinde Onifade <onifkay@gmail.com>
2022-08-03 10:34:07 +0100

    Add addBook functionality

--- js/index.js
+++ js/index.js
@@ -14,4 +14,22 @@ class Book {
     this.author = author;
     this.id = id;
   }
+
+  addBook() {
+    const { title, author, id } = this;
+    const bookList = { title, author, id };
+    books = JSON.parse(localStorage.getItem('books'));
+    if (title === '' || author === '') {
+      errorMsg('All fields are required');
+    } else if (books !== null) {
+      books.push(bookList);
+      localStorage.setItem('books', JSON.stringify(books));
+      books = JSON.parse(localStorage.getItem('books'));
+    } else {
+      books = [];
+      books.push(bookList);
+      localStorage.setItem('books', JSON.stringify(books));
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+  }
 }

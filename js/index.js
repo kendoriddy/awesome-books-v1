0964a1106bab107089f656dc331b5256df5c8eb5
Kehinde Onifade <onifkay@gmail.com>
2022-08-04 10:29:18 +0100

    page navigation finalized

--- js/index.js
+++ js/index.js
@@ -97,13 +97,23 @@ if (books !== null) {
 function handlePageChange(e) {
   const navLinks = document.getElementsByClassName('nav-link');
   for (let i = 0; i < navLinks.length; i += 1) {
-    navLinks[i].style.color = '#343434';
+    navLinks[i].style.color = '';
   }
 
   if (e.target.classList.contains('list')) {
     document.querySelector('#book-display').style.display = 'block';
     document.querySelector('#add-book').style.display = 'none';
     document.querySelector('#contact').style.display = 'none';
+  }
+  if (e.target.classList.contains('addBook')) {
+    document.querySelector('#book-display').style.display = 'none';
+    document.querySelector('#add-book').style.display = 'block';
+    document.querySelector('#contact').style.display = 'none';
+  }
+  if (e.target.classList.contains('contactInfo')) {
+    document.querySelector('#book-display').style.display = 'none';
+    document.querySelector('#add-book').style.display = 'none';
+    document.querySelector('#contact').style.display = 'block';
   }
 }
 

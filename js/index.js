fbb00ff9ad9d17cc6f02356bb52b52dbe0b7f7a8
Kehinde Onifade <onifkay@gmail.com>
2022-08-04 10:35:57 +0100

    Add date and time function

--- js/index.js
+++ js/index.js
@@ -120,3 +120,63 @@ function handlePageChange(e) {
 document.addEventListener('click', (e) => {
   handlePageChange(e);
 });
+
+// Date
+const timeElement = document.querySelector('.time');
+const dateElement = document.querySelector('.date');
+
+/**
+ * @param {Date} date
+ */
+function formatTime(date) {
+  const hours12 = date.getHours() % 12 || 12;
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const isAm = date.getHours() < 12;
+
+  return `${hours12.toString().padStart(2, '0')}:${minutes
+    .toString()
+    .padStart(2, '0')}:${seconds
+    .toString()
+    .padStart(2, '0')} ${isAm ? 'AM' : 'PM'}`;
+}
+
+/**
+ * @param {Date} date
+ */
+function formatDate(date) {
+  const DAYS = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  return `${DAYS[date.getDay()]}, ${
+    MONTHS[date.getMonth()]
+  } ${date.getDate()} ${date.getFullYear()}`;
+}
+
+setInterval(() => {
+  const now = new Date();
+
+  timeElement.textContent = formatTime(now);
+  dateElement.textContent = formatDate(now);
+}, 200);
